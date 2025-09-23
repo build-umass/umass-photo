559bcc7a905d0f30525b3bb0cedeab08fdac2597
Maxwell Tang <amoinus@gmail.com>
2025-09-23 19:07:03 -0400

    chore: refactor admin page

--- src/app/admin-page/page.tsx
+++ src/app/admin-page/page.tsx
@@ -1,7 +1,5 @@
 "use client"
-
-import { useEffect, useState } from "react";
-import AuthorizedPage from "./AuthorizedPage";
+import { ChangeEvent, useEffect, useRef, useState } from "react"
 
 enum PageState {
     DEFAULT,
@@ -9,6 +7,89 @@ enum PageState {
     AUTHORIZED,
     UNAUTHORIZED
 }
+
+type User = {
+    id: string,
+    username: string,
+    email: string,
+    bio: string,
+    role: string,
+}
+
+function UserManagementRow(props: {
+    deleteUser: () => void;
+    userReference: User;
+}) {
+    const [isModified, setIsModified] = useState(false);
+    const [role, setRole] = useState(props.userReference.role);
+    const setRoleRich = (e: ChangeEvent<HTMLInputElement>) => {
+        setRole(e.target.value);
+        setIsModified(true)
+    }
+    return <div>Hi, I am a row!!!!
+        <div className="flex gap-2">
+            {isModified ? <div>*</div> : <></>}
+            <div>{props.userReference.id}</div>
+            <div>{props.userReference.username}</div>
+            <div>{props.userReference.email}</div>
+            <div>{props.userReference.bio}</div>
+            <input className="bg-amber-200" value={role} onChange={setRoleRich}></input>
+            <button className="bg-red-500" onClick={props.deleteUser}>DELETE</button>
+        </div>
+        <div><pre>{JSON.stringify(props.userReference)}</pre></div>
+    </div>
+}
+
+function UserManagementTab() {
+    const [isLoaded, setIsLoaded] = useState(false);
+    useEffect(() => {
+        (async () => {
+            const response = await fetch("/api/get-user-all");
+            const data = await response.json();
+            configurationData.current = data;
+            setIsLoaded(true)
+        })()
+    }, [])
+    const configurationData = useRef([]);
+    if (!isLoaded) {
+        return <>loading...</>
+    } else {
+        return <>
+        {configurationData.current.map((row, index) => {
+            return <UserManagementRow key={index} userReference={row} deleteUser={() => {}}></UserManagementRow>;
+        })}
+        </>
+
+    }
+}
+
+function EventManagementTab() {
+    return <>event</>
+}
+
+enum Tab {
+    USER,
+    EVENT
+}
+
+function AdminPageContent() {
+    const [tab, setTab] = useState<Tab>(Tab.USER)
+    const currentTab =
+        tab == Tab.USER ? <UserManagementTab></UserManagementTab> :
+            tab == Tab.EVENT ? <EventManagementTab></EventManagementTab> :
+                <div></div>
+
+    return <div>
+        <div>
+            <button onClick={() => setTab(Tab.USER)}>Users</button>
+            <button onClick={() => setTab(Tab.EVENT)}>Events</button>
+        </div>
+        <div>
+            {currentTab}
+        </div>
+    </div>
+}
+
 export default function AdminPage() {
     const [pageState, setPageState] = useState<PageState>(PageState.DEFAULT);
     useEffect(
@@ -37,7 +118,7 @@ export default function AdminPage() {
         case PageState.UNAUTHENTICATED:
             return <div>I don&apost know who you are</div>
         case PageState.AUTHORIZED:
-            return <AuthorizedPage></AuthorizedPage>
+            return <AdminPageContent></AdminPageContent>
         case PageState.UNAUTHORIZED:
             return <div>You can&apost see this</div>
     }
