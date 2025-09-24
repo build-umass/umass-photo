b2a70b3072258dff3bed6ab932af3eee970d618c
Maxwell Tang <amoinus@gmail.com>
2025-09-24 11:36:55 -0400

    feat: add update-user-data endpoint

---
+++ src/app/api/update-user-data/route.ts
@@ -0,0 +1,25 @@
+import dotenv from "dotenv";
+import { NextRequest } from "next/server";
+import { attachCookies, getUserClient } from "@/app/utils/supabase/client";
+
+dotenv.config();
+
+type User = {
+  id: string,
+  username: string,
+  email: string,
+  bio: string,
+  role: string,
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export async function GET(request: NextRequest) {
+  const client = getUserClient(request);
+
+  const { toDelete, toModify }: { toDelete: User["id"][], toModify: User[] } = await request.json();
+  await client.from("photoclubuser").upsert(toModify);
+  await client.from("photoclubuser").delete().in("id", toDelete);
+
+  const response = new Response();
+  return attachCookies(client, response);
+}
