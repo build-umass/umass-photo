75f0eb323174e042c730f4b2562ae79cace5baa2
Maxwell Tang <amoinus@gmail.com>
2025-09-24 15:13:47 -0400

    fix: add validation to get-role endpoint

--- src/app/api/get-role/route.ts
+++ src/app/api/get-role/route.ts
@@ -8,11 +8,13 @@ dotenv.config();
 export async function GET(request: NextRequest) {
   const client = getUserClient(request);
 
-  const { data: userData } = await client.auth.getUser();
-  const id = userData.user?.id;
+  const { data: {user} } = await client.auth.getUser();
+  if (!user) return new Response("{}");
+  const id = user.id;
   const { data: roleObjectList } = await client.from("photoclubuser").select("photoclubrole(*)").eq("id", id);
-  if (!roleObjectList) return;
+  if (!roleObjectList) return new Response("{}");
   const [{ photoclubrole: roleData }] = roleObjectList
+
   const response = new Response(JSON.stringify(roleData));
   return attachCookies(client, response);
 }
