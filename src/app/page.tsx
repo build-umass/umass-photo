0bf8bb6124da27fa12ccb8cbb74ad3da010d81bc
maxwell3025 <amoinus@gmail.com>
2025-09-30 18:29:18 -0400

    refactor: replaced footer with common variation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 import { faYoutube } from "@fortawesome/free-brands-svg-icons";
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import Navbar from './components/navbar/navbar';
+import Footer from './components/footer/footer';
 
 
 export default function Home() {
@@ -73,67 +74,8 @@ export default function Home() {
             </div>
           </div>
         </div>
-
       </main>
-
-      <footer className = "bg-gray-200">
-        <div className="max-w-screen-xl mx-auto px-3 py-15">
-          <div className="flex flex-wrap justify-between">
-            <div className="">
-              <h3 className="font-Jaldi font-bold text-[#971B2F] text-3xl mb-4">Umass Photo</h3>
-              <div className="flex space-x-6 hover:underline mb-4">
-                <a className = "text-gray-500 hover:text-gray-700" href="#"><FontAwesomeIcon icon={faFacebook} /></a>
-                <a className = "text-gray-500 hover:text-gray-700" href="#"><FontAwesomeIcon icon={faLinkedin} /></a> 
-                <a className = "text-gray-500 hover:text-gray-700" href="#"><FontAwesomeIcon icon={faYoutube} /></a>
-                <a className = "text-gray-500 hover:text-gray-700" href="#"><FontAwesomeIcon icon={faInstagram} /></a>              
-              </div>
-              <p className="text-gray-400 text-base font-normal">Website © BUILD Umass 2025</p>
-            </div> 
-            <div className="ml-auto flex space-x-20">
-              <div className="">
-                <h3 className="font-Jaldi font-bold text-lg mb-6">Gallery</h3>
-                <div className="flex flex-col space-y-2">
-                  <a className= "font-Jaldi block text-gray-700 hover:underline mb-4" href = "#">All Photos</a>
-                  <a className = "font-Jaldi block text-gray-700 hover:underline" href = "#">Submit Your photos</a>
-                </div>
-              </div>
-              <div className="">
-                <h3 className="font-Jaldi font-bold text-lg mb-6">Events</h3>
-                <div className="flex flex-col space-y-2">
-                  <div className="font-Jaldi flex flex-col space-y-2">
-                    <a className= "font-Jaldi block text-gray-700 hover:underline mb-4" href = "#">Spring contest</a>
-                    <a className= "font-Jaldi block text-gray-700 hover:underline mb-4" href = "#">Fall Foliage Contest</a>
-                    <a className="font-Jaldi block text-gray-700 hover:underline" href = "#">Summer Contest</a>
-                  </div>
-                </div>
-              </div>
-              <div className="">
-              <h3 className="font-Jaldi font-bold text-lg mb-6">Mission</h3>
-                <div className="flex flex-col space-y-2">
-                  <div className="flex flex-col space-y-2">
-                    <a className= "font-Jaldi block text-gray-700 hover:underline mb-4" href = "#">Our Story</a>
-                    <a className="font-Jaldi block text-gray-700 hover:underline" href = "#">E-Board</a>
-                  </div>
-                </div>
-              </div>
-              <div className="">
-              <h3 className="font-Jaldi font-bold text-lg mb-6">Contact</h3>
-                <div className="flex flex-col space-y-2">
-                  <div className="flex flex-col space-y-2">
-                    <a className= "font-Jaldi block text-gray-700 hover:underline mb-4" href = "#">Instagram</a>
-                    <a className= "font-Jaldi block text-gray-700 hover:underline mb-4" href = "#">Email</a>
-                    <a className="font-Jaldi block text-gray-700 hover:underline" href = "#">Page</a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-      </footer>
-    
-
-
+    <Footer></Footer>
     </div> 
   );
 }
