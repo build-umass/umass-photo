8cb1355398a19990ea5e9f5c92805a57a1e34899
maxwell3025 <amoinus@gmail.com>
2025-10-14 14:45:17 -0400

    refactor: format

--- sql/permissions.sql
+++ sql/permissions.sql
@@ -1,26 +1,43 @@
-CREATE POLICY "Enable insert for users based on user_id"
-ON "public"."photo"
-AS PERMISSIVE
-FOR
-INSERT WITH CHECK (
-  (SELECT auth.uid()) = authorid
-);
+CREATE POLICY "Enable insert for users based on user_id" ON "public"."photo" AS PERMISSIVE FOR
+INSERT
+  WITH CHECK (
+    (
+      SELECT
+        auth.uid()
+    ) = authorid
+  );
 
-CREATE POLICY "Allow users to create their own profiles if they set safe roles"
-ON "public"."photoclubuser"
-AS PERMISSIVE
-FOR INSERT
-TO public
-WITH CHECK (
-  ((SELECT auth.uid()) = id) and
-  ((SELECT is_admin FROM "public"."photoclubrole" WHERE roleid = role) = false)
-);
+CREATE POLICY "Allow users to create their own profiles if they set safe roles" ON "public"."photoclubuser" AS PERMISSIVE FOR
+INSERT
+  TO public WITH CHECK (
+    (
+      (
+        SELECT
+          auth.uid()
+      ) = id
+    )
+    AND (
+      (
+        SELECT
+          is_admin
+        FROM
+          "public"."photoclubrole"
+        WHERE
+          roleid = role
+      ) = false
+    )
+  );
 
-CREATE POLICY "Only allow eboard to create events"
-ON "event"
-AS PERMISSIVE
-FOR ALL
-TO authenticated
-USING (
-  (SELECT "role" FROM "public"."photoclubuser" WHERE (SELECT auth.uid()) = id) = 'eboard'
+CREATE POLICY "Only allow eboard to create events" ON "event" AS PERMISSIVE FOR ALL TO authenticated USING (
+  (
+    SELECT
+      "role"
+    FROM
+      "public"."photoclubuser"
+    WHERE
+      (
+        SELECT
+          auth.uid()
+      ) = id
+  ) = 'eboard'
 );
