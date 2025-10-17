4aa6d69a6f8b3d832b64b30794f95dc5a64c95ae
maxwell3025 <amoinus@gmail.com>
2025-10-17 14:01:05 -0400

    refactor: dry tab system

--- src/app/admin-page/AdminPageContent.tsx
+++ src/app/admin-page/AdminPageContent.tsx
@@ -1,38 +1,29 @@
 import UserManagementTab from "./UserManagementTab"
 import EventManagementTab from "./EventManagementTab"
-import { useState } from "react"
+import { ReactNode, useState } from "react"
 import AdminPageTabButton from "./AdminPageTabButton"
 
-enum Tab {
-    USER,
-    EVENT
-}
+type TabInfo = Readonly<{
+    title: string;
+    component: () => ReactNode;
+}>;
 
-function getTabValues(): Tab[] {
-    return Object.values(Tab).filter(x => typeof x === "number")
-}
-
-function getTabName(tab: Tab): string {
-    switch (tab) {
-        case Tab.USER:
-            return "Users"
-        case Tab.EVENT:
-            return "Events"
-    }
-}
+const userTab =  { title: "Users",  component: UserManagementTab  };
+const eventTab = { title: "Events", component: EventManagementTab };
+const tabs: ReadonlyArray<TabInfo> = [userTab, eventTab];
 
 export default function AdminPageContent() {
-    const [activeTab, setActiveTab] = useState<Tab>(Tab.USER)
-    const currentTab =
-        activeTab == Tab.USER ? <UserManagementTab></UserManagementTab> :
-            activeTab == Tab.EVENT ? <EventManagementTab></EventManagementTab> :
-                <div></div>
-
+    const [activeTab, setActiveTab] = useState<TabInfo>(userTab)
+    const currentTab = activeTab.component();
 
     return <div className="flex">
         <div className="flex flex-col bg-umass-red w-96">
-            {getTabValues().map(tab => {
-                return <AdminPageTabButton key={tab} onClick={() => setActiveTab(tab)} highlighted={tab == activeTab}>{getTabName(tab)}</AdminPageTabButton>
+            {tabs.map(tab => {
+                return <AdminPageTabButton
+                    key={tab.title}
+                    onClick={() => setActiveTab(tab)}
+                    highlighted={tab == activeTab}
+                >{tab.title}</AdminPageTabButton>
             })}
         </div>
         <div>
