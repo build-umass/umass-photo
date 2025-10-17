83c653f118d2a12e7a817771d1a341ead42b5ed6
maxwell3025 <amoinus@gmail.com>
2025-10-17 17:31:01 -0400

    feat: add EventChip Element

---
+++ src/app/components/EventChip/index.tsx
@@ -0,0 +1,6 @@
+export default function EventChip() {
+    return <div className="fixed inset-0 bg-black/25 flex items-center justify-center">
+        <div className="w-2/3 h-2/3 bg-white rounded-2xl">
+        </div>
+    </div>
+}
--- src/app/components/page.tsx
+++ src/app/components/page.tsx
@@ -1,6 +1,7 @@
 import Navbar from "./navbar/navbar"; // Import Navbar from the navbar folder
 import Footer from "./footer/footer"; // Import Footer from the footer folder
 import MarkdownElement from "./markdown/MarkdownElement";
+import EventChip from "./EventChip";
 
 const sampleText = `
 ---
@@ -255,6 +256,7 @@ export default function ComponentsPreview() {
     <div className="flex flex-col min-h-screen">
       <Navbar />
       <MarkdownElement content={sampleText}></MarkdownElement>
+      <EventChip></EventChip>
       <Footer />
     </div>
   );
