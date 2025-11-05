8851af44f431bd2be5a156f5f608f0b91a5d6970
maxwell3025 <amoinus@gmail.com>
2025-11-05 11:36:29 -0500

    feat: unauthenticated upload feature

---
+++ src/app/api/upload-photo/route.ts
@@ -0,0 +1,38 @@
+import { randomBytes } from 'crypto';
+import dotenv from "dotenv";
+import { NextRequest } from "next/server";
+import { attachCookies, getUserClient } from "@/app/utils/supabase/client";
+
+dotenv.config();
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export async function POST(request: NextRequest) {
+  const client = getUserClient(request);
+
+  const formData = await request.formData();
+  const image = formData.get("image") as (File | null)
+  if (!image) {
+    throw new Error("File missing")
+  }
+  const extension = image.name.split(".").pop()
+  const fileName = `${randomBytes(20).toString('base64url')}.${extension}`;
+  console.log(`fileName: ${fileName}`);
+  console.log(image);
+
+  const { data: _storageUploadData, error: storageUploadError } = await client.storage.from("photos").upload(fileName, image);
+  if (storageUploadError) {
+    throw storageUploadError
+  }
+
+  console.log("success");
+
+  const userId = (await client.auth.getUser()).data?.user?.id
+  if (!userId) {
+    throw new Error("userId could not be found")
+  }
+  await client.from("photo").insert({ authorid: userId, file: fileName, postdate: new Date(Date.now()).toISOString() })
+
+
+  const response = new Response("", { status: 201 });
+  return attachCookies(client, response);
+}
--- src/app/photo-gallery/UploadChip/index.tsx
+++ src/app/photo-gallery/UploadChip/index.tsx
@@ -1,24 +1,39 @@
+'use client'
+import { FormEvent } from "react";
+import { useFormStatus } from "react-dom";
+
 export default function UploadChip({
     closeCallback
-} : {
+}: {
     closeCallback: () => void
 }) {
+    async function uploadPhoto(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault()
+
+        const formData = new FormData(event.currentTarget)
+        const response = await fetch('/api/upload-photo', {
+            method: 'POST',
+            body: formData,
+        })
+        if (response.status != 201) {
+            throw Error("Bad Upload!")
+        }
+    }
+
     return <div className="fixed inset-0 flex items-center justify-center bg-black/20">
         <div className="w-2/3 h-2/3 bg-white p-12">
             <form
-                action="/urlencoded?firstname=sid&lastname=sloth"
-                method="POST"
-                encType="multipart/form-data"
+                onSubmit={uploadPhoto}
                 className="flex flex-col gap-4 h-full box-border"
             >
                 <input type="file" name="image" className="bg-gray-200 p-3 border-2 border-dashed border-gray-600 grow"></input>
                 <input type="text" name="title" className="bg-gray-200 p-3 rounded-xl text-3xl font-bold" placeholder="Title"></input>
                 <textarea name="description" className="bg-gray-200 p-3 rounded-xl grow" placeholder="description"></textarea>
                 <div className="flex flex-row">
-                {/* TODO convert to common button style */}
-                <button onClick={closeCallback}>Close</button>
-                <button>Preview</button>
-                <button type="submit">Upload</button>
+                    {/* TODO convert to common button style */}
+                    <button onClick={closeCallback}>Close</button>
+                    <button>Preview</button>
+                    <button type="submit">Upload</button>
                 </div>
             </form>
         </div>
