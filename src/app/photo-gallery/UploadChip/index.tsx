626377b721cc8f86b131cffce6e6873b9bdece9b
maxwell3025 <amoinus@gmail.com>
2025-11-15 14:16:00 -0500

    feat: tag add frontend

--- src/app/photo-gallery/UploadChip/index.tsx
+++ src/app/photo-gallery/UploadChip/index.tsx
@@ -1,6 +1,6 @@
 'use client'
 import UmassPhotoButton from "@/app/components/UmassPhotoButton";
-import { FormEvent, useRef, useState } from "react";
+import { FormEvent, useEffect, useRef, useState } from "react";
 import PreviewPage from "./PreviewPage";
 
 export default function UploadChip({
@@ -11,6 +11,17 @@ export default function UploadChip({
     const [previewMode, setPreviewMode] = useState(false);
     const imageField = useRef<HTMLInputElement | null>(null);
     const [imageDataURL, setImageDataUrl] = useState("");
+    const [tagOptions, setTagOptions] = useState<Set<string>>(new Set())
+    const [selectedTags, setSelectedTags] = useState<string[]>([])
+
+    async function refreshTagList() {
+        const response = await fetch('/api/get-tag-all', {
+            method: 'GET',
+        })
+        const tags = new Set(await response.json() as string[]);
+        setTagOptions(tags);
+    }
+
     async function uploadPhoto(event: FormEvent<HTMLFormElement>) {
         event.preventDefault()
 
@@ -28,6 +39,27 @@ export default function UploadChip({
         }
     }
 
+    function addTag(tag: string) {
+        setSelectedTags((selectedTags) => {
+            if (selectedTags.includes(tag)) {
+                return selectedTags
+            } else {
+                return [...selectedTags, tag]
+            }
+        });
+    }
+
+    function removeTag(tagToRemove: string) {
+        setSelectedTags((selectedTags) => {
+            return selectedTags.filter(tag => tag !== tagToRemove);
+        });
+    }
+
+    useEffect(() => {
+        refreshTagList()
+    }, []);
+
+
     async function processFileLoad() {
         const selectedImage = imageField.current?.files?.item(0)
         if (!selectedImage) return;
@@ -38,6 +70,12 @@ export default function UploadChip({
         }
         fr.readAsDataURL(selectedImage);
     }
+
+    const tagList = selectedTags.map(tag => <div key={tag} className="bg-gray-300 rounded-xl py-1 px-4 text-2xl">{tag}
+        <button type="button" onClick={() => { removeTag(tag) }}>X</button>
+    </div>)
+
+    const tagOptionElements = Array.from(tagOptions).map(tag => <option key={tag} value={tag}>{tag}</option>)
 
     return <div className="fixed inset-0 flex items-center justify-center bg-black/20">
         <div className="w-2/3 h-2/3 bg-white p-12">
@@ -52,6 +90,13 @@ export default function UploadChip({
                 <input type="file" name="image" className="bg-gray-200 p-3 border-2 border-dashed border-gray-600 grow" required ref={imageField} onChange={processFileLoad}></input>
                 <input type="text" name="title" className="bg-gray-200 p-3 rounded-xl text-3xl font-bold" placeholder="Title" required></input>
                 <textarea name="description" className="bg-gray-200 p-3 rounded-xl grow" placeholder="description" required></textarea>
+                <div className="flex flex-row gap-3">
+                    {tagList}
+                    <select defaultValue="default" className="bg-gray-300 rounded-xl py-1 px-4 text-2xl" onChange={(e) => { if (e.target.value !== "default") addTag(e.target.value) }}>
+                        <option value="default">+</option>
+                        {tagOptionElements}
+                    </select>
+                </div>
                 <div className="flex flex-row gap-3">
                     {/* TODO convert to common button style */}
                     <UmassPhotoButton className="bg-gray-400" type="button" onClick={closeCallback}>Close</UmassPhotoButton>
