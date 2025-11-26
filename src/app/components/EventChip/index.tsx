48795f4335ccc62dd0fbd4126618f9057264554a
maxwell3025 <amoinus@gmail.com>
2025-11-26 14:51:14 -0500

    feat: made all event fields unser-selectable

--- src/app/components/EventChip/index.tsx
+++ src/app/components/EventChip/index.tsx
@@ -1,13 +1,25 @@
 import { Tables } from "@/app/utils/supabase/database.types"
 import Image from "next/image"
 
+/**
+ * Converts a Date object into the [datetime-local](https://developer.mozilla.org/en-US/docs/Web/HTML/Guides/Date_and_time_formats#local_date_and_time_strings) format
+ * @param date the date to be converted.
+ */
+function dateToDateTimeLocalString(date: Date) {
+    // Source - https://stackoverflow.com/a/61082536
+    // Posted by neurino, modified by community. See post 'Timeline' for change history
+    // Retrieved 2025-11-25, License - CC BY-SA 4.0
+    date = new Date(date);
+    date.setMinutes(date.getMinutes() - date.getTimezoneOffset());
+    return date.toISOString().slice(0, 16);
+}
+
 export default function EventChip({
     eventData,
 }: {
     eventData: Tables<"event">
 }) {
-    const startTime = new Date(eventData.startdate)
-    const endTime = new Date(eventData.enddate)
+    console.debug(new Date(Date.now()).toString());
     return <div className="fixed inset-0 bg-black/25 flex items-center justify-center">
         <div className="w-2/3 h-2/3 bg-gray-200 rounded-xl flex flex-col">
             <div className="overflow-hidden relative h-64">
@@ -20,20 +32,32 @@ export default function EventChip({
                 ></Image>
             </div>
             <div className="grow p-9 flex flex-col">
-                <h1 className="text-4xl font-bold p-5 rounded-xl bg-gray-300">{eventData.name}</h1>
-                <div className="text-2xl px-5 flex gap-1 items-baseline pt-1 pb-4">
-                    <div className="font-bold">Date:</div>
-                    <div className="px-5 py-1 rounded-xl bg-gray-300">{startTime.toDateString()}</div>
-                    <div>|</div>
-                    <div className="px-5 py-1 rounded-xl bg-gray-300">{startTime.toTimeString()}</div>
-                    <div>&ndash;</div>
-                    <div className="px-5 py-1 rounded-xl bg-gray-300">{endTime.toDateString()}</div>
-                    <div>|</div>
-                    <div className="px-5 py-1 rounded-xl bg-gray-300">{endTime.toTimeString()}</div>
+                <input className="text-4xl font-bold p-5 rounded-xl bg-gray-300"></input>
+                <div className="text-2xl px-5 flex gap-1 items-center pt-1 pb-4">
+                    <div className="font-bold">
+                        Date:
+                    </div>
+                    <input
+                        className="px-2 rounded-xl bg-gray-300"
+                        type="datetime-local"
+                        id="start-time-input"
+                        name="start-time"
+                        defaultValue={dateToDateTimeLocalString(new Date())}
+                    />
+                    <div>
+                        &ndash;
+                    </div>
+                    <input
+                        className="px-2 rounded-xl bg-gray-300"
+                        type="datetime-local"
+                        id="end-time-input"
+                        name="end-time"
+                    />
+                    <div className="text-sm ml-auto">
+                        Current Timezone: {Intl.DateTimeFormat().resolvedOptions().timeZone}
+                    </div>
                 </div>
-                <div className="text-2xl px-5 py-4 rounded-xl bg-gray-300">
-                    {eventData.description}
-                </div>
+                <textarea className="text-2xl px-5 py-4 rounded-xl bg-gray-300"></textarea>
             </div>
             <div className="px-14 py-5 flex justify-between">
                 <button className="px-8 py-1 font-bold text-2xl rounded-xl cursor-pointer bg-gray-400 text-white">Close</button>
