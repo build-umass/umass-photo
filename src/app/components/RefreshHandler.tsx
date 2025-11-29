f6d2a92d43e5fc4237173221d5ec84fcc4370f43
maxwell3025 <amoinus@gmail.com>
2025-11-29 00:02:04 -0500

    fix: add correct refresh login logic

--- src/app/components/RefreshHandler.tsx
+++ src/app/components/RefreshHandler.tsx
@@ -8,21 +8,26 @@ const REFRESH_MARGIN_MS = 60000
 
 export default function RefreshHandler() {
     useEffect(() => {
-        let refreshTimeout = 0;
         let breakRefreshHook = () => {};
+        let refreshTimeout = 0;
+
         const refreshTask = async () => {
             while (true) {
                 const loginExpiryTime = localStorage.getItem("loginExpiryTime")
                 if (loginExpiryTime === null) return;
-                await new Promise<boolean>(res => {
-                    const resumeRefreshLoop = () => {res(true)}
+                const millisUntilRefresh = parseInt(loginExpiryTime) - Date.now() - REFRESH_MARGIN_MS
+
+                const shouldBreak = await new Promise<boolean>(res => {
+                    const resumeRefreshLoop = () => {res(false)}
                     const breakRefreshLoop = () => {res(true)}
                     breakRefreshHook = breakRefreshLoop
-                    const millisUntilRefresh = parseInt(loginExpiryTime) - Date.now() - REFRESH_MARGIN_MS
                     refreshTimeout = window.setTimeout(resumeRefreshLoop, millisUntilRefresh)
                 })
                 breakRefreshHook = () => {};
                 refreshTimeout = 0;
+
+                if (shouldBreak) break;
+
                 const response = await fetch("/api/refresh", { method: "POST" });
                 const expiry = await response.json();
                 localStorage.setItem("loginExpiryTime", (expiry.expires_at * 1000).toString())
