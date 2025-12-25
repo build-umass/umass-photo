4ed4ccbf34ad40dfe701687b85b0e100ed20360c
maxwell3025 <amoinus@gmail.com>
2025-12-25 14:54:41 -0500

    ci: update build target

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,6 @@ const SUPABASE_URL = process.env.SUPABASE_URL;
 if (!SUPABASE_URL)
   throw new Error("SUPABASE_URL is not defined in environment variables");
 const SUPABASE_HOST_NAME = SUPABASE_URL.split("/")[2];
-console.log("Supabase Host Name:", SUPABASE_HOST_NAME);
 
 const nextConfig: NextConfig = {
   images: {
