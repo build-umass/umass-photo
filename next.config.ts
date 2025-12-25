d43ae25eb382b1e99dc1c62d8448fb6717855512
maxwell3025 <amoinus@gmail.com>
2025-12-25 14:25:54 -0500

    refactor: add manual check to configuration

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,20 @@
 import type { NextConfig } from "next";
-import dotenv from "dotenv"
-dotenv.config()
+import dotenv from "dotenv";
+dotenv.config();
 
-const SUPABASE_HOST_NAME = process.env.SUPABASE_URL?.split("/")[2]
+const SUPABASE_URL = process.env.SUPABASE_URL;
+if (!SUPABASE_URL)
+  throw new Error("SUPABASE_URL is not defined in environment variables");
+const SUPABASE_HOST_NAME = SUPABASE_URL?.split("/")[2];
 
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        hostname: SUPABASE_HOST_NAME!
-      }
-    ]
-  }
+        hostname: SUPABASE_HOST_NAME!,
+      },
+    ],
+  },
 };
 
 export default nextConfig;
