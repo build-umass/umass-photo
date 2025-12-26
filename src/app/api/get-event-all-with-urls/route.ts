3d59aa1e29b05662cd4fff94b01dad568fa0d7a7
maxwell3025 <amoinus@gmail.com>
2025-12-26 18:28:25 -0500

    feat: add better error handling in routes

--- src/app/api/get-event-all-with-urls/route.ts
+++ src/app/api/get-event-all-with-urls/route.ts
@@ -4,12 +4,13 @@ import { attachCookies, getUserClient } from "@/app/utils/supabase/client";
 export async function GET(request: NextRequest) {
   const client = getUserClient(request);
 
-  const {
-    data: { user },
-  } = await client.auth.getUser();
-  if (!user) return new Response("[]");
-  const { data: eventList } = await client.from("event").select("*");
-  if (!eventList) return new Response("[]");
+  const { data: eventList, error: selectEventError } = await client.from("event").select("*");
+  if (selectEventError) {
+    return new Response(JSON.stringify({
+      message: "Failed to fetch events",
+      error: selectEventError,
+    }), { status: 500 });
+  }
 
   // Transform the data and get image URLs
   const eventListWithURLs = eventList.map((event) => {
