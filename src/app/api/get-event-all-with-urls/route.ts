a65ee12f7aaeddea88f9aee325e34b8062b0ffb6
maxwell3025 <amoinus@gmail.com>
2025-12-30 10:24:43 -0500

    refactor: format

--- src/app/api/get-event-all-with-urls/route.ts
+++ src/app/api/get-event-all-with-urls/route.ts
@@ -4,12 +4,17 @@ import { attachCookies, getUserClient } from "@/app/utils/supabase/client";
 export async function GET(request: NextRequest) {
   const client = getUserClient(request);
 
-  const { data: eventList, error: selectEventError } = await client.from("event").select("*");
+  const { data: eventList, error: selectEventError } = await client
+    .from("event")
+    .select("*");
   if (selectEventError) {
-    return new Response(JSON.stringify({
-      message: "Failed to fetch events",
-      error: selectEventError,
-    }), { status: 500 });
+    return new Response(
+      JSON.stringify({
+        message: "Failed to fetch events",
+        error: selectEventError,
+      }),
+      { status: 500 },
+    );
   }
 
   // Transform the data and get image URLs
