ace736bb2c8b6cea22f1738cc0a9ad6577c47f0d
maxwell3025 <amoinus@gmail.com>
2026-01-13 18:20:14 -0500

    feat: add 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <>
+      <h1 className="mx-auto mt-auto text-2xl">404 - Page Not Found</h1>
+      <p className="mx-auto mb-auto">
+        Sorry, the page you are looking for does not exist.
+      </p>
+    </>
+  );
+}
