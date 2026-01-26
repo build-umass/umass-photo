4314db79fefaaf17ef91b508a86b6d68e3995e0f
maxwell3025 <amoinus@gmail.com>
2026-01-26 17:29:27 -0500

    feat: fix title styling in blogs

--- src/app/blogs/BlogContent.tsx
+++ src/app/blogs/BlogContent.tsx
@@ -5,7 +5,7 @@ import { Tables } from "../utils/supabase/database.types";
 export default function BlogContent({ blog }: { blog: Tables<"blog"> }) {
   return (
     <div className="text-lg">
-      <h1 className="text-umass-red mx-auto mb-4 text-3xl font-bold">
+      <h1 className="text-umass-red mx-auto mb-4 text-center text-3xl font-bold">
         {blog.title}
       </h1>
       <MarkdownContent>{blog.content}</MarkdownContent>
