b5ebd2f4ab34b66dfd9ac7bec69f58bbe034c4b5
maxwell3025 <amoinus@gmail.com>
2026-01-31 15:41:00 -0500

    feat: make admin page bounce users to login

--- src/app/admin-page/layout.tsx
+++ src/app/admin-page/layout.tsx
@@ -1,11 +1,20 @@
+"use server";
 import { ReactNode } from "react";
 import AdminPageTabSelector from "./AdminPageTabSelector";
+import { createClient } from "../utils/supabase/server";
+import { redirect } from "next/navigation";
 
 export default async function AdminPageLayout({
   children,
 }: Readonly<{
   children: ReactNode;
 }>) {
+  const client = await createClient();
+
+  if (!(await client.rpc("has_good_role")).data) {
+    redirect("/login");
+  }
+
   return (
     <div className="flex min-h-0 shrink grow basis-0">
       <AdminPageTabSelector></AdminPageTabSelector>
